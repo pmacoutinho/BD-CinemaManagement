2978ba7caaf3a7486cc14fcfb46ad98e818d8cbf
João Martins <jferreiram2000@gmail.com>
2021-06-17 22:11:11 +0100

    sql file to drop all tables 'constraint-aware'.

---
+++ sql/ieeta_db/drop_tables.sql
@@ -0,0 +1,18 @@
+use p3g8;
+
+drop table operations.Ticket;
+drop table operations.Client;
+drop table operations.Reservation;
+drop table operations.Session_instance;
+drop table operations.Cleaning_Record;
+
+
+drop table management.Room;
+drop table management.Employee;
+drop table management.Employee_Type;
+drop table management.Employee_Shift;
+
+drop table data.Session;
+drop table data.Film;
+
+drop table location.Cinema;
